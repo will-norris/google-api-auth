8d9c1e6f05a10da046ce8009522d1ce5c114dd24
William Norris <william.norris@artsalliancemedia.com>
2023-11-20 22:00:15 +0000

    Use rotating handler

--- src/google_api_auth/setup_logging.py
+++ src/google_api_auth/setup_logging.py
@@ -1,11 +1,12 @@
 import logging
+import logging.handlers # https://stackoverflow.com/questions/64951836/python-logging-attributeerror-module-logging-has-no-attribute-handlers
 
 fmt = logging.Formatter('%(name)s -> %(asctime)s || %(message)s')
 
 def setup_logger(logger_name):
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(fmt)
-    file_handler = logging.FileHandler(f'{logger_name}.log', mode='w')
+    file_handler = logging.handlers.TimedRotatingFileHandler(f'{logger_name}.log', when='midnight', backupCount=3)
     file_handler.setFormatter(fmt)
 
     logger = logging.getLogger(f'{logger_name}')
